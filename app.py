ef126616c66e915dbcdc9c546834db8d27320db7
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-04-18 14:45:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,10 +107,4 @@ def hello():
     return ""
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
